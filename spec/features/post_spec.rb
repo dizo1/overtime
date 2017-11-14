8617e01af756b90d55e1003369b7dbd657aec358
Muganda <mugandadizo@gmail.com>
2017-11-14 12:31:44 +0000

    implemented user connection to posts

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -7,7 +7,35 @@ describe 'navigate' do
         end
         it 'has a title of Posts' do
             visit posts_path
-            expect(page).to have_content(/Post/)
+            expect(page).to have_content(/Posts/)
+        end
+    end
+    
+        describe 'creation' do
+            before do
+            user =  @user = User.create(email: "test@test.com", password:"123456", password_confirmation: "123456", first_name: "jon", last_name: "doe")  
+            login_as(user, :scope => :user)
+            visit new_post_path
+          
+        end
+        
+        it 'has a new form that can be reached' do
+            expect(page.status_code).to eq(200)
+        end
+        
+        it 'can be created from new form page' do
+            fill_in 'post[date]', with: Date.today
+            fill_in 'post[rationale]', with: "Dragons Rationale"
+            click_on "Save"
+        
+            expect(page).to have_content("Dragons Rationale")
+        end
+        
+        it 'will have a user associated with it' do
+            fill_in 'post[date]', with: Date.today
+            fill_in 'post[rationale]', with: "User_Association"
+            click_on "Save" 
+            expect(User.last.posts.last.rationale).to eq("User Association")
         end
     end
 end
